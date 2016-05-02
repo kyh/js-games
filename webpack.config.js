9a10a1d3477d4295c7c2afd876d58d54959205ad
Kaiyu Hsu <im.kaiyu@gmail.com>
2016-05-02 11:05:18 -0700

    Setup webpack hot reload

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const DEV_PORT = 8000;
+
+module.exports = {
+  entry: [
+    `webpack-dev-server/client?http://localhost:${DEV_PORT}`,
+    'webpack/hot/dev-server',
+    './app.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/static/',
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  port: DEV_PORT
+};
